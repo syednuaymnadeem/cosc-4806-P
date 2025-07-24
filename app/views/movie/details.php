42312e624aded278ef33cf5b85c4b4b510ec39ef
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-07-24 10:57:02 +0000

    Displaying the movie poster image if a valid poster URL is available in the movie data.

--- app/views/movie/details.php
+++ app/views/movie/details.php
@@ -4,3 +4,7 @@
 
 <?php endif; ?>
 <h2><?= htmlspecialchars($movie['Title']) ?> (<?= htmlspecialchars($movie['Year']) ?>)</h2>
+
+<?php if ($movie['Poster'] && $movie['Poster'] !== "N/A"): ?>
+    <img src="<?= htmlspecialchars($movie['Poster']) ?>" alt="Poster" style="max-width:200px;">
+<?php endif; ?>
