377bae17fa18066a48e7c6ef590b60fa0474646a
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-07-24 09:26:17 +0000

    Added a simple if statement in index.php to call the appropriate controller action based on the URL.

--- index.php
+++ index.php
@@ -6,3 +6,7 @@ require_once '../core/Controller.php';
 
 require_once '../app/controllers/MovieController.php';
 require_once '../app/models/MovieModel.php';
+
+// If no route given → show search
+$controller = new MovieController();
+if (!isset($_GET['action']))
