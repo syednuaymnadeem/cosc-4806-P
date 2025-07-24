490e5382a587f7f6b17ae5224ecbb043ba4e3213
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-07-24 09:18:37 +0000

    Includes the Router file in index.php so that we can handle URL routing for the application.

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-session_start();  
+// public/index.php
 
-// TEMP SESSION FIX FOR TESTING
-$_SESSION['auth'] = 1;
-$_SESSION['userid'] = 1;
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once 'app/init.php';
-
-$app = new App;
+require_once '../core/Router.php';
