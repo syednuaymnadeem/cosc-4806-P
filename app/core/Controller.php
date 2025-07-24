e8b3ae97444f8feea2d568ce92a5d9fd605810fb
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-07-24 09:33:10 +0000

    Added model() method to Controller to load and return the requested model class.

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,9 +2,15 @@
 // core/Controller.php
 
 class Controller {
-public function view($view, $data = []) {
-    extract($data);
-    require "../app/views/layout/header.php";
-    require "../app/views/{$view}.php";
-    require "../app/views/layout/footer.php";
+    public function view($view, $data = []) {
+        extract($data);
+        require "../app/views/layout/header.php";
+        require "../app/views/{$view}.php";
+        require "../app/views/layout/footer.php";
+    }
+
+    public function model($model) {
+        require_once "../app/models/{$model}.php";
+        return new $model();
+    }
 }
