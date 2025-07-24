42edd599049f1b0c1d17710935085e81b0a2b803
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-07-24 10:34:16 +0000

    Added required movie title input field to search form

--- app/views/movie/search.php
+++ app/views/movie/search.php
@@ -4,4 +4,8 @@
 
 <form method="GET" action="">
   <input type="hidden" name="action" value="details">
+  <label>
+      Movie Title:
+      <input type="text" name="title" required>
+  </label><br><br>
 </form>
