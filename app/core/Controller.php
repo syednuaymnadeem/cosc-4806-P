4b483974dc56c940cea70ea3d50866c983e33ce5
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-07-24 09:32:14 +0000

    Added view() method to the Controller to load the specified view with data and including the header and footer.

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,5 +2,9 @@
 // core/Controller.php
 
 class Controller {
-  
+public function view($view, $data = []) {
+    extract($data);
+    require "../app/views/layout/header.php";
+    require "../app/views/{$view}.php";
+    require "../app/views/layout/footer.php";
 }
